5cf96a596c80b90ceb2fe8df125d174935d0c336
phil <phil@phil-desktop>
2019-11-12 21:04:06 -0700

    First add for spring boot csv batch application.

---
+++ src/main/java/com/spring/howto/CsvbatchdemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.howto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvbatchdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvbatchdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/howto/CsvbatchdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.howto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvbatchdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
